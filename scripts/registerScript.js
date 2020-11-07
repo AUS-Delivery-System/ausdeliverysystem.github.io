54f5f040ae527bb61649bcb27fa9f79e2baaa72d
moekharoof <b00073865@aus.edu>
2020-11-08 00:10:16 +0400

    0 progress

---
+++ scripts/registerScript.js
@@ -0,0 +1,166 @@
+window.onload = function () {
+    document.getElementById("vError").style.display = "none";
+    document.getElementById("bError").style.display = "none";
+    document.getElementById("oError").style.display = "none";
+    document.getElementById("volunteerForm").style.display = "none";
+    document.getElementById("foodForm").style.display = "none";
+    document.getElementById("bankForm").style.display = "none";
+    document.getElementById("navbarToggler").style.display = "none";
+    document.getElementById("stop_it").style.display = "none";
+    var counter = 0;
+    $("#addrow").on("click", function () {
+        document.getElementById("stop_it").style.display = "none";
+        var newRow = $("<tr>");
+        var cols = "";
+        cols += '<td class="col-sm-5 text-center"><input type="text"  required class="form-control" name="name' + counter + '"/></td>';
+        cols += '<td class="col-sm-5 text-center"><input type="text" required class="form-control" name="address' + counter + '"/></td>';
+        cols += '<td class="col-sm-2 text-center"><input type="button" class="ibtnDel btn btn-md btn-danger" id="delete" value="Delete"></td>';
+        newRow.append(cols);
+        $("table.order-list").append(newRow);
+        counter++;
+    });
+
+
+
+    $("table.order-list").on("click", ".ibtnDel", function (event) {
+        if (counter == 0) {
+            document.getElementById("stop_it").style.display = "block";
+        } else {
+            $(this).closest("tr").remove();
+            counter--;
+        }
+    });
+
+    function calculateRow(row) {
+        var price = +row.find('input[name^="price"]').val();
+
+    }
+
+    function calculateGrandTotal() {
+        var grandTotal = 0;
+        $("table.order-list").find('input[name^="price"]').each(function () {
+            grandTotal += +$(this).val();
+        });
+        $("#grandtotal").text(grandTotal.toFixed(2));
+    }
+
+}
+
+function registerVolunteer() {
+    if (document.getElementById("volunteerForm").style.display == "block") {
+        document.getElementById("volunteerForm").style.display = "none";
+    } else {
+        if (document.getElementById("foodForm").style.display == "block") {
+            document.getElementById("foodForm").style.display = "none";
+        }
+        if (document.getElementById("bankForm").style.display == "block") {
+            document.getElementById("bankForm").style.display = "none";
+        }
+        document.getElementById("volunteerForm").style.display = "block";
+    }
+}
+
+function registerOutlet() //1
+{
+    if (document.getElementById("foodForm").style.display == "block") {
+        document.getElementById("foodForm").style.display = "none";
+    } else {
+        if (document.getElementById("volunteerForm").style.display == "block") {
+            document.getElementById("volunteerForm").style.display = "none";
+        }
+        if (document.getElementById("bankForm").style.display == "block") {
+            document.getElementById("bankForm").style.display = "none";
+        }
+        document.getElementById("foodForm").style.display = "block";
+    }
+}
+
+function registerFoodBank() {
+    if (document.getElementById("bankForm").style.display == "block") {
+        document.getElementById("bankForm").style.display = "none";
+    } else {
+        if (document.getElementById("volunteerForm").style.display == "block") {
+            document.getElementById("volunteerForm").style.display = "none";
+        }
+        if (document.getElementById("foodForm").style.display == "block") {
+            document.getElementById("foodForm").style.display = "none";
+        }
+        document.getElementById("bankForm").style.display = "block";
+    }
+}
+
+function showItems() {
+    if (document.getElementById("navbarToggler").style.display == "block") {
+        document.getElementById("navbarToggler").style.display = "none";
+    } else
+        document.getElementById("navbarToggler").style.display = "block";
+}
+
+function vValidate() {
+    var msg;
+    let x = (document.getElementById("vPasswordConfirm").val() == document.getElementById("vPassword").val());
+    if (!x) {
+        alert();
+        document.getElementById("vPasswordConfirm").css("border", "red solid 1px");
+
+    }
+}
+
+function validateInfo() {
+    var flag = true;
+    var msg = "";
+    //clear
+    var firstLetter;
+    if (document.getElementById("bankForm").style.display == "block") {
+        firstLetter = "b";
+    } else if (document.getElementById("volunteerForm").style.display == "block") {
+        firstLetter = "v";
+    } else if (document.getElementById("foodForm").style.display == "block") {
+        firstLetter = "o";
+
+    }
+
+    let gg = '<strong>Error!</strong> ';
+    console.log("In function");
+    console.log(document.getElementById(firstLetter + "Password").value);
+    if (document.getElementById(firstLetter + "Password").value != document.getElementById(firstLetter + "PasswordConfirm").value) {
+        document.getElementById(firstLetter + "Error").style.display = "block";
+        msg += "Passwords do not match. ";
+        flag = false;
+    }
+
+    let temp = document.getElementById(firstLetter + "Password").value;
+
+    if (temp.length < 8) {
+        document.getElementById(firstLetter + "Error").style.display = "block";
+        msg += "Password must be atleast 8 characters. ";
+        flag = false;
+    }
+    if (!(temp.match(/[a-z]/) && temp.match(/[A-Z]/) && temp.match(/[0-9]/))) {
+        document.getElementById(firstLetter + "Error").style.display = "block";
+        msg += "Passwords must include lower-case letter, upper-case letter, and number.";
+
+        flag = false;
+    }
+
+    if (!flag) {
+        alert("W");
+        $("#" + firstLetter + "passError").html(gg + msg);
+    } else {
+        if (document.getElementById("foodForm").style.display == "block") {
+
+            document.getElementById("foodForm").submit();
+        } else if (document.getElementById("volunteerForm").style.display == "block") {
+
+            document.getElementById("volunteerForm").submit();
+        } else if (document.getElementById("bankForm").style.display == "block") {
+
+            document.getElementById("bankForm").submit();
+        }
+    }
+}
+
+
+function validateAll() {
+    vValidate();
+}
